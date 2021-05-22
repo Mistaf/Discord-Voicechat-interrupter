a7c2cfb04f2fe13bc8df53c3ed72c75382706658
Mistaf <21952929+Mistaf@users.noreply.github.com>
2021-05-22 04:33:45 +0200

    Made the bot...

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-vc interrupter
+# Vc Interrupter
+
+A discord to interrupt people when they are talking.
+
+## Available commands
+
+<sup><sub>The commands can only be used by "masters"</sup></sub>
+
+- `/join <id/me>`
+    - Joins channel to interrupt the people
+    - The id has to be the channel id of what channel you want the bot to join
+    You can also type `me` instead of a id for the bot to join your current channel (if you are in any)
+- `/leave <id/me>`
+    - Leaves the channel because it isn't funny anymore.
+    - The id once again has to be the channel id or `me` for your channel
+- `/ignore <user>`
+    - Stops to interrupt people who are ignored
+    - `<user>` can either be the users nickname, username, mention, user id
+- `/unignore <user>`
+    - Starts to interrupt the ignored person again
+    - `<user>` can either be the users nickname, username, mention, user id
+
+## Inviting the bot
+
+First you have to create a new application [here](https://discord.com/developers/applications).
+When you created the application first of all save the application id for later since you need that to invite the bot and then you have to make it a bot by going to the bot tab in the navigation at the left side and press the "Add Bot" button.
+
+Now you have a bot you need to get the token, click on "Click to Reveal Token" and copy that token (make sure never to share it with anyone because that token gives full control over the bot)
+
+Now to actually invite the bot go to [this](https://discordapi.com/permissions.html) site and select what permissions you want to give the bot. Its advised to give the bot permissions to atleast `Connect` and `Mute Members` to unmute itself when server muted.
+You could also just give it Administrator so it has permissions to join every channel
+
+## Setting up the bot
+
+
+Make sure you got [Node.js](https://nodejs.org/) version 12.0.0 or higher installed and type in a console (while being in the same directory as the bot): `npm install`, that will install all the required packages.
+
+To start the bot use `node ./index.js`.
+
+To close to the bot press ctrl + c
+
+
+## Built With
+
+* [Discord.js](https://discord.js.org/) - Powerful node.js module that allows you to interact with [Discord API](https://discordapp.com/developers/docs/intro)
+* [Node.js](https://nodejs.org/) - Node.js® is a JavaScript runtime built on [Chrome's V8 JavaScript engine](https://v8.dev/).
+
+
+## Author
+
+* [**Mistaf**](https://github.com/Mistaf)
---
+++ index.js
@@ -0,0 +1,181 @@
+const fs = require('fs');
+createFiles();
+const Discord = require("discord.js");
+const bot = new Discord.Client({presence:{status: 'invisible' },disableMentions:"all"});
+const config = require("./config.json");
+let sound = "sound.mp3";
+bot.login(config.token);
+
+bot.on('ready',()=>{
+    console.log("bot is ready");
+});
+ignorelist = JSON.parse(fs.readFileSync('./ignorelist.json'));
+bot.on('message', message =>{
+    console.log(message.content);
+    if(!config.masters.includes(message.author.id))return;
+    const args = message.content.split(' ').slice(1);
+    if(message.content.toLowerCase().startsWith("/join")){
+        const channel = getChannelID(message,args[0]);
+        if(channel)
+            channel.join();
+        else message.reply(`Could not find channel with the id \`${channelID}\``);
+    }
+    else if(message.content.toLowerCase().startsWith("/leave")){
+        const channel = getChannelID(message,args[0]);
+        if(channel)
+            channel.leave();
+        else message.reply(`im not in the channel with the id \`${channelID}\``);
+    }
+
+    else if(message.content.toLowerCase().startsWith('/ignore')){
+        let userid = getUserId(message);
+        if(userid){
+            ignorelist.push(userid);
+            fs.writeFileSync("./ignorelist.json",JSON.stringify(ignorelist,null,'\t'), (err) => {if (err) console.error(err)});
+            message.reply(`Added userid ${userid} to the ignore list`); 
+        }
+    }
+    else if(message.content.toLowerCase().startsWith('/unignore')){
+        let userid = getUserId(message);
+        if(userid){
+            ignorelist=ignorelist.filter(id=>id!=userid);
+            fs.writeFileSync("./ignorelist.json",JSON.stringify(ignorelist,null,'\t'), (err) => {if (err) console.error(err)});
+            message.reply(`removed userid ${userid} from the ignore list`); 
+        }
+    }
+    else if(message.content.toLowerCase().startsWith('/sound')){
+        let tempSound = args.join(' ');
+        if(fs.existsSync(`./sounds/${tempSound}`)){
+            sound=tempSound;
+            message.reply(`Interrupt sound changed to \`${sound}\``);
+        }else
+            message.reply(`Cannot find ${tempSound} in the sounds folder`);
+    }
+
+});
+
+var joinBAck = {};
+bot.on("voiceStateUpdate",(oldState, newState)=>{
+  //AUTO UNMUTE
+    if(newState.id==bot.user.id){
+        if(newState.serverDeaf.valueOf(1))newState.setDeaf(0);
+        if(newState.serverMute.valueOf(1))newState.setMute(0);
+    }
+    if(oldState.channelID!=newState.channelID&&speakers[oldState.channelID])
+        speakers[oldState.channelID]=speakers[oldState.channelID].filter(e=>oldState.member.id!=e);
+    
+    
+    //AUTO JOIN BACK + RESET SPEAKERS OBJECT
+    if(newState.id==bot.user.id){
+        if(!newState.channel){
+            if(activeChannels[oldState.channelID]){
+                activeChannels[oldState.channelID].destroy();
+                delete activeChannels[oldState.channelID];
+            }
+            return;
+        }
+        //pause song mid move
+        if(activeChannels[oldState.channelID])
+            activeChannels[oldState.channelID].pause();
+
+        if(oldState.channel&&oldState.channel!=newState.channel){
+
+            speakers[oldState.channel.id]=[];
+
+            if(!joinBAck[newState.guild.id]){joinBAck[newState.guild.id]=true;
+                if(oldState.channel)oldState.channel.join();
+            }
+            else if(joinBAck[newState.guild.id])joinBAck[newState.guild.id]=false;
+        }
+
+        if(newState.member.id == bot.user.id)
+            newState.channel.members.forEach(member=>{
+                speaking(member,member.voice.speaking)
+            })
+    }
+});
+
+
+bot.on("guildMemberSpeaking",(member,isSpeaking)=>{
+    speaking(member,isSpeaking.bitfield);
+});
+
+function getChannelID(message,arg){
+    if(arg.toLowerCase() == "me"&&message.member)
+        return message.member.voice.channel;
+    return bot.channels.cache.find(channel => channel.id==arg);
+}
+
+function getUserId(message){
+    let user=message.mentions.users.first();
+    let arg = message.content.split(" ").slice(1).join(' ');
+    if(user){
+        return user.id;
+    }else{
+        user = message.guild.members.cache.find(member =>member.displayName.toLowerCase()==arg.toLowerCase()||member.user.username.toLowerCase()==arg.toLowerCase());
+        if(user)
+            return user.id
+        return arg;
+    }
+}
+
+var speakers = {};
+function speaking(member,speaking){
+    if(member.id==bot.user.id||member.bot||config.masters.includes(member.id)||ignorelist.includes(member.id))return;
+    if(!speakers[member.voice.channelID])
+    speakers[member.voice.channelID]=[];
+
+    if(speaking)//add to array of users in this channel that are speaking, if array is includes 1 speak,
+        speakers[member.voice.channelID].push(member.id);
+    else//remove from array of users in this channel that are speaking, if array is includes 0 stop speak,
+        speakers[member.voice.channelID]=speakers[member.voice.channelID].filter(e=>member.id!=e);
+
+    // console.log(speakers);
+    try {
+        speak(member.voice.channel,speakers[member.voice.channelID].length > 0);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+activeChannels = {};
+function speak(channel,play){
+    const vc = bot.voice.connections.find(connection => connection.channel.id == channel.id);
+    if(!vc) return;
+
+    if(activeChannels[channel.id]){
+        if(play)
+            activeChannels[channel.id].resume();
+        else
+            activeChannels[channel.id].pause(true); //true makes it so it doesnt stop sending audio packets so it can instantly continue to talk
+        
+    }
+    else{
+        if(!play)return;
+        activeChannels[channel.id] = vc.play(`./sounds/${sound}`);
+
+        activeChannels[channel.id].on('finish', ()=>{
+            activeChannels[channel.id].destroy();
+            delete activeChannels[channel.id];
+            speak(channel,play);
+        });
+        
+    }
+
+}
+
+function createFiles(){
+    let newFile = false;
+    if(!fs.existsSync("./config.json")){
+        fs.writeFileSync("./config.json", JSON.stringify({"token":"<ENTER BOT TOKEN HERE>","masters":["USERID OF MASTER","FOR MULTIPLE PUT A COMMA AT THE END OF A LINE"]},null,"\t"), { flag: 'wx' });
+        newFile=true;
+        console.log("Config created!",'fill in the missing data in the config.json');
+    }
+    if(!fs.existsSync("./ignorelist.json")){
+        fs.writeFileSync("./ignorelist.json", JSON.stringify([],null,"\t"), { flag: 'wx' });
+        newFile=true;
+        console.log("Ignorelist created!");
+    }
+
+    if(newFile)process.exit();
+}
